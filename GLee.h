9ab6e699db075f583cc2f0b03ae18ae2a85a8d0a
the-e <fabianw2@mail.upb.de>
2010-05-24 02:07:29 +0200

    Slight adjustments for compatibility. wx needs to have wx/msw/winundef.h included, and GLee 5.4 changed a few calling conventions.
    Signed-off-by: the-e <fabianw2@mail.upb.de>

--- GLee.h
+++ GLee.h
@@ -55,6 +55,8 @@
 #ifdef WIN32
 	#define WIN32_LEAN_AND_MEAN
 	#include <windows.h>
+ //Added for compatibility
+	#include <wx/msw/winundef.h>
 	#include <GL/gl.h>
 #elif defined(__APPLE__) || defined(__APPLE_CC__)
     #define GL_GLEXT_LEGACY
