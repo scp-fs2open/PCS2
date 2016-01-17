9055bf247fd928dc01415e4749445997594c80fe
asarium <asarium@gmail.com>
2016-01-17 12:22:03 +0100

    Enable poition independent code for Unix

--- cmake/platform-unix.cmake
+++ cmake/platform-unix.cmake
@@ -3,4 +3,6 @@ INCLUDE(util)
 
 MESSAGE(STATUS "Configuring UNIX specific things and stuff...")
 
+set(CMAKE_POSITION_INDEPENDENT_CODE TRUE)
+
 target_compile_definitions(platform INTERFACE UNIX HAVE_SYS_TYPES_H HAVE_SYS_STAT_H HAVE_UNISTD_H)
